cd329eddf45b352bea3be03b70fa6ef4c19ab2b2
Jefferson Isla <142628931+Jeffersonisla88@users.noreply.github.com>
2024-09-18 15:17:16 -0500

    Quichua rimashun 3
    En este repositorio podrán observar y descargar a version 3 de mi App

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Quichua-Rimashun-3
-TFM versión 3
+# sistemaquichua
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Tema1.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="phoneme-container">
+      <h2>Kichwa Phonemes</h2>
+      <div class="phoneme-group">
+        <h3>Consonants</h3>
+        <ul>
+          <li v-for="phoneme in consonantPhonemes" :key="phoneme">{{ phoneme }}</li>
+        </ul>
+      </div>
+      <div class="phoneme-group">
+        <h3>Semiconsonants</h3>
+        <ul>
+          <li v-for="phoneme in semiconsonantPhonemes" :key="phoneme">{{ phoneme }}</li>
+        </ul>
+      </div>
+      <div class="phoneme-group">
+        <h3>Vowels</h3>
+        <ul>
+          <li v-for="phoneme in vowelPhonemes" :key="phoneme">{{ phoneme }}</li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        consonantPhonemes: ['p', 't', 'k', '*q', 'č', 'h', 's', 'š', 'm', 'n', 'ñ', 'l', 'l y', 'r'],
+        semiconsonantPhonemes: ['w', 'y'],
+        vowelPhonemes: ['i', 'u', 'a']
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  .phoneme-container {
+    /* Add your desired styling here */
+  }
+  .phoneme-group {
+    /* Add your desired styling here */
+  }
+  </style>
